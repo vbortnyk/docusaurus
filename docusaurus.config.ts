0ee5c57238005a075765b6b25ecff50a5b6957c1
vbortnyk <viacheslav.bortnyk@gmail.com>
2026-07-21 10:42:06 +0200

    config: update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: process.env.DEPLOYMENT_URL ?? "https://vbortnyk.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL ?? "/docusaurus/",
+  baseUrl: process.env.BASE_URL ?? "/portfilio/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
